b745bc5d410120a71f699cc8327304f6ef308291
Mateus Evair Souza Macedo <mateus.evair@wivenn.com.br>
2022-08-30 16:42:42 -0300

    Fake users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Matheus Dias',
+            'email' => 'mthdias@outlook.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123@Mudarr'),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
